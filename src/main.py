a3c8a3d920920e366a9a4705a0a5fe561b97e488
cjlynch278 <c.lynch278@gmail.com>
2022-05-20 12:15:16 -0600

    Add logging and reformat

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from src.SQLOperations import SQLOperations
 from src.Collibra_Operations import Collibra_Operations
 import yaml
 import os
+import logging
+from datetime import datetime
 
 
 class MainClass:
@@ -26,10 +28,20 @@ class MainClass:
             self.schema = "extract"
             self.environment = config["ENVIRONMENT"]["gore"]
             self.auth = config["AUTH"]["auth-header"]
+            self.logger_location = config["LOGGER"]["LOCATION"]
+
         except KeyError:
             print("The config file is incorrectly setup")
             os._exit(1)
-
+        logging.basicConfig(
+            filename=self.logger_location + "_" + str(datetime.today().date()) + ".log",
+            filemode="a",
+            format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+            datefmt="%H:%M:%S",
+            level=logging.DEBUG,
+        )
+        logging.info("App started")
+        logging.info("Config File Read")
         self.sql_operations = SQLOperations(
             self.sql_user,
             self.sql_password,
@@ -40,16 +52,20 @@ class MainClass:
             self.admin_only_domain_id,
             self.environment,
         )
+        logging.info("Sql operations setup")
         self.sql_operations.connect_to_sql()
-        print(self.sql_operations.read_sql())
+        logging.info("SQL connected")
 
         self.collibra_operations = Collibra_Operations(
             self.admin_only_domain_id, self.environment, self.token_auth
         )
+        logging.info("Collibra Operations setup")
 
     def run(self):
         dataframe = self.sql_operations.read_sql()
+        logging.info("Sql read successfully")
         self.collibra_operations.create_assets_and_attributes(dataframe)
+        logging.info("Assets and attributes created")
 
 
 if __name__ == "__main__":
