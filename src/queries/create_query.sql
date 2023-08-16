02b8f5d7202114b9c2e2e20f633d6f54b710b2e5
cjlynch278 <c.lynch278@gmail.com>
2023-08-16 12:35:13 -0600

    fix format again

--- src/queries/create_query.sql
+++ src/queries/create_query.sql
@@ -7,19 +7,7 @@
         left join
         (select * from collibra.collibra_assets where is_current=1 and
         Domain_ID in ('{0}', '{1}')) collibra_assets on SN_System_ID =
-        collibra_assets.name where collibra_assets.Name is null and {2}.is_current = 1
+        collibra_assets.name where collibra_assets.Name is null and {3}.is_current = 1
 
-        Union
 
-        select
-        [SN_System_ID], sn_services.[Name] asset_name,[CI_Number],[Install_Status],
-        [Business_Criticality] ,[URL],[Owned_By],[IT_Owner],[Supported_By],
-        [Export_Control],[Legal_Hold],[APM_Data_Sensitivity],[Disaster_Recovery_Gap],[Records_Retention], [Description]
-        , [CI_Type]
-        from servicenow.servicenow_cmbd_ci_service
-        as sn_services
-        left Join
-        (select * from collibra.collibra_assets where is_current=1 and
-        Domain_ID in ('{0}', '{1}')) collibra_assets on SN_System_ID =
-        collibra_assets.name where collibra_assets.Name is null and sn_services.is_current = 1
 
